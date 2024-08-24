590d8508e5b29b6cae0d3c66478be76170acc8a5
Val-in <valeriia.vp1@yandex.ru>
2024-08-24 10:42:16 +0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculator
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator_Logic calculator = new Calculator_Logic();
+            var additionalResult = calculator.Additional(5, 7);
+            Console.WriteLine("Additional: {0}", additionalResult);
+
+            var subtractionResult = calculator.Subtraction(2, 8);
+            Console.WriteLine("Subtraction: {0}", subtractionResult);
+
+            var multiplicationResult = calculator.Multiplication(6, 3);
+            Console.WriteLine("Multiplication: {0}", multiplicationResult);
+
+            var divisionResult = calculator.Division(6, 3);
+            Console.WriteLine("Division: {0}", divisionResult);
+
+            Console.ReadKey();
+        }
+    }
+}
